ae404faadc5394945776ef4a95ab2dcee2acbe31
roamingengineers <164939430+roamingengineers@users.noreply.github.com>
2024-06-01 04:38:41 +0000

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,10 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+ :heart:.
 
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 st.header("Test Homepage")
